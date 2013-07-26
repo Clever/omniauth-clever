8b0bfbf023389e50a12487c8986f4f58323383a4
Carol Nichols <cnichols@thinkthroughmath.com>
2013-07-26 13:30:56 -0400

    Changes #fetch to an or for the purpose of setting the default for the clever_landing param to admin
    Turns out that options.client_options isn't a Hash, it's an instance of OmniAuth::Strategy::Options, which is a subclass of Hashie::Mash, which doesn't seem to be doing what I expect with #fetch.

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -15,7 +15,7 @@ module OmniAuth
       def authorize_params
         super.tap do |params|
           params[:scope] = 'read_only'
-          params[:clever_landing] = options.client_options.fetch(:clever_landing, 'admin')
+          params[:clever_landing] = options.client_options.clever_landing || 'admin'
         end
       end
 
