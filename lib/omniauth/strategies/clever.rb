80534afed036e862d4fcb094b7eea889e2524f5c
Taylor Singletary <taylor.singletary@clever.com>
2015-08-03 14:07:53 -0700

    To better support Clever-initiated logins, only validate state when login is initiated by the client.

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -4,13 +4,21 @@ require 'base64'
 module OmniAuth
   module Strategies
     class Clever < OmniAuth::Strategies::OAuth2
-      option :name, "clever"
+      # Clever is a unique OAuth 2.0 service provider in that login sequences
+      # are often initiated by Clever, not the client. When Clever initiates
+      # login, a state parameter is not relevant nor sent.
 
+      option :name, "clever"
       option :client_options, {
         :site          => 'https://api.clever.com',
         :authorize_url => 'https://clever.com/oauth/authorize',
         :token_url     => 'https://clever.com/oauth/tokens'
       }
+
+      # This option bubbles up to the OmniAuth::Strategies::OAuth2
+      # when we call super in the callback_phase below.
+      # **State will still be verified** when login is initiated by the client.
+      option :provider_ignores_state, true
 
       def token_params
         super.tap do |params|
@@ -18,7 +26,21 @@ module OmniAuth
         end
       end
 
-
+      def callback_phase
+        error = request.params["error_reason"] || request.params["error"]
+        stored_state = session.delete("omniauth.state")
+        if error
+          fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+        else
+          # Only verify state if we've initiated login and have stored a state
+          # to compare to.
+          if stored_state && (!request.params["state"] || request.params["state"] != stored_state)
+            fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+          else
+            super
+          end
+        end
+      end
 
       uid{ raw_info['data']['id'] }
 
