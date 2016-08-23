ae551408f9c3b17c0b6d3ad4f096d582edc66d3d
Joseph Chen <joseph.chen@gmail.com>
2016-08-22 19:04:06 -0700

    Fix unknown redirect uri bug by NOT appending the query string to the callback url.

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -57,6 +57,11 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/me').parsed
       end
+
+      # Fix unknown redirect uri bug by NOT appending the query string to the callback url.
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
