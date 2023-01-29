92d63249d9ffa5f77ae1b3435f6c68a8fc11cd23
Ryan Montgomery <rmontgomery429@gmail.com>
2023-01-29 16:35:22 -0500

    Allow the redirect_uri to be passed in as an option

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -60,7 +60,7 @@ module OmniAuth
 
       # Fix unknown redirect uri bug by NOT appending the query string to the callback url.
       def callback_url
-        full_host + script_name + callback_path
+        options[:redirect_uri] || (full_host + script_name + callback_path)
       end
     end
   end
